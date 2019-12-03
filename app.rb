27170ad64459d32af6666f80c0a2b346a746730d
Sharon Watkins <swatkins876@gmail.com>
2019-12-03 17:20:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,4 +17,13 @@ class App < Sinatra::Base
    @phrase = params[:phrase]
    erb :say
  end
+
+ get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @w1 = params[:word1]
+    @w2 = params[:word2]
+    @w3 = params[:word3]
+    @w4 = params[:word4]
+    @w5 = params[:word5]
+    erb :saywords
+  end
 end
